3d632af0c52c5b4efc3f4d620632567fd4eac295
nahee-park <nahee.developer@gmail.com>
2022-10-14 00:26:19 +0900

    feat: remove ibc

--- src/components/Detail/DetailPortfolio.tsx
+++ src/components/Detail/DetailPortfolio.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import IBC from '@src/assets/Icon/IBC.svg';
+import React from 'react';
+
 import Title from '../CreateSpace/Title';
 
 function DetailPortfolio() {
   return (
     <section className="flex flex-col justify-center items-center">
-      <div className="flex flex-row justify-between w-[98%] mt-[37.5px] mb-[32.5px]">
+      {/* <div className="flex flex-row justify-between w-[98%] mt-[37.5px] mb-[32.5px]">
         <Title>My Tokens</Title>
         <Title>1,000.00 TEL</Title>
-      </div>
-      <IBC className="mb-[106px]" />
+      </div> */}
+      {/* <IBC className="mb-[106px]" /> */}
     </section>
   );
 }
